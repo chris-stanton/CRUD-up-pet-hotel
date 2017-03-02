7a77a47108c0310dd60fa87b452a3a2df9803468
Catherine J <cjarocki@gmail.com>
2017-03-02 13:49:02 -0600

    adding save and delete functions

--- newpet.js
+++ newpet.js
@@ -2,28 +2,23 @@ var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var config = {
-  database: 'phi', // the name of the database
-  host: 'localhost', // where is your database
-  port: 5432, // the port number for your database
-  max: 10, // how many connections at one time
-  idleTimeoutMillis: 30000 // 30 seconds to try to connect
+  database: 'phi',
+  host: 'localhost',
+  port: 5000,
+  max: 10,
+  idleTimeoutMillis: 30000
 };
 
 var pool = new pg.Pool(config);
 
-
 router.post('/new', function(req, res){
-  // This will be replaced with an INSERT statement to SQL
   var newPet = req.body;
 
   pool.connect(function(errorConnectingToDatabase, client, done){
     if(errorConnectingToDatabase) {
-      // There was an error connecting to the database
       console.log('Error connecting to database: ', errorConnectingToDatabase);
       res.sendStatus(500);
     } else {
-      // We connected to the database!!!
-      // Now, we're gonna' git stuff!!!!!
       client.query('INSERT INTO pets (name, breed, color) VALUES ($1, $2, $3);',
         [newPet.name, newPet.breed, newPet.color],
         function(errorMakingQuery, result){
@@ -52,6 +47,30 @@ router.put('/save/:id', function(req, res){
       client.query('UPDATE pets SET name=$1, breed=$2, color=$3;',
         [petObject.name, petObject.breed, petObject.color, petId],
         function(errorMakingQuery, result){
+          done();
+          if(errorMakingQuery) {
+            console.log('Error making the database query: ', errorMakingQuery);
+            res.sendStatus(500);
+          } else {
+            res.sendStatus(202);
+          }
+        });
+    }
+  });
+});
+
+router.delete('/delete/:id', function(req, res){
+  var petId = req.params.id;
+  console.log('pet id to delete', petId);
+//connecting to and deleting row from db
+  pool.connect(function(errorConnectingToDatabase, client, done){
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to database: ', errorConnectingToDatabase);
+      res.sendStatus(500);
+    } else {
+      client.query('DELETE FROM pets WHERE id=$1;', //SQL query
+        [bookId],
+        function(errorMakingQuery, result){ //function that runs after query takes place
           done();
           if(errorMakingQuery) {
             console.log('Error making the database query: ', errorMakingQuery);
