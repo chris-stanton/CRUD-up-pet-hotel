c12444dbdf35eb6ba05f1343d3a93444d9507411
Paige Bystrom <pdbystrom@gmail.com>
2017-03-02 16:32:36 -0600

    appending to dom

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,54 +1,63 @@
 console.log("this works");
 $(document).ready(function(){
 
-//registure button click listener
-$("#register").on("click", function(){
-  event.preventDefault();
-  var firstInput = $("#firstName").val();
-  var lastInput = $("#lastName").val();
-  var fullName = firstInput + " " + lastInput;
+  //add pets into table on the DOM
+  $('tbody').append('<td>owner name</td>' +
+  '<td><input type="text" placeholder="owner"/></td>' +
+  '<td><input type="text" placeholder="breed"/></td>' +
+  '<td><input type="text" placeholder="color"/></td>' +
+  '<td><button>GO</button></td>' +
+  '<td><button>GO</button></td>' +
+  '<td><button>IN</button></td>');
 
-  $("#ownerContainer").append('<p>' + fullName + '</p>');
+  //registure button click listener
+  $("#register").on("click", function(){
+    event.preventDefault();
+    var firstInput = $("#firstName").val();
+    var lastInput = $("#lastName").val();
+    var fullName = firstInput + " " + lastInput;
 
-var ownerName = {
-  firstName: firstInput,
-  lastName: lastInput
-};
+    $("#ownerContainer").append('<p>' + fullName + '</p>');
 
-    $.ajax({
-      type: 'POST',
-      url: '/ownername'
-      data: ownerName
-      success: function() {
+    var ownerName = {
+      firstName: firstInput,
+      lastName: lastInput
+    };
 
-      }
-    })
-});//end of registure listener
-
-
-//addpet button click listener
-$("#addPetButton").on("click", function(){
-  event.preventDefault();
-  var petName = $("#petName").val();
-  var petColor = $("#petColor").val();
-  var petBreed = $("#breed").val();
-
-  $("#petNameContainer").append('<p>' + petName + '</p>');
-  $("#petBreedContainer").append('<p>' + petColor + '</p>');
-  $("#petColorContainer").append('<p>' + petBreed + '</p>');
-});//end of addpet listener
+    //     $.ajax({
+    //       type: 'POST',
+    //       url: '/ownername'
+    //       data: ownerName
+    //       success: function() {
+    //
+    //       }
+    //     })
+    });//end of registure listener
 
 
+    //addpet button click listener
+    $("#addPetButton").on("click", function(){
+      event.preventDefault();
+      var petName = $("#petName").val();
+      var petColor = $("#petColor").val();
+      var petBreed = $("#breed").val();
+
+      $("#petNameContainer").append('<p>' + petName + '</p>');
+      $("#petBreedContainer").append('<p>' + petColor + '</p>');
+      $("#petColorContainer").append('<p>' + petBreed + '</p>');
+    });//end of addpet listener
 
 
 
 
 
-$("table").on("click", ".goButton", function() { // change .goButton if name is different
-  var thisPetId = $(this).parent().parent().children()
-  $.ajax({
-    type: 'GET',
-    url: '/save/' + thisPetId
-  })
-}); //end on go button click
-});//end of doc.ready
+
+    // button to save pet edits
+    $("table").on("click", ".goButton", function() { // change .goButton if name is different
+      var thisPetId = $(this).parent().parent().children()
+      $.ajax({
+        type: 'GET',
+        url: '/save/' + thisPetId
+      });
+    }); //end on go button click
+  });//end of doc.ready
