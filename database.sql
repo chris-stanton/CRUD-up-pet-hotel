01ecba114aa9e395cfea79cf0b9fb406307b872a
Catherine J <cjarocki@gmail.com>
2017-03-02 13:22:22 -0600

    Update database.sql

--- database.sql
+++ database.sql
@@ -1,1 +1,22 @@
 # CRUD-up-pet-hotel
+
+CREATE TABLE owners (
+    id SERIAL PRIMARY KEY,
+    first_name character varying(60),
+    last_name character varying(80)
+);
+
+CREATE TABLE pets (
+    id SERIAL PRIMARY KEY,
+    name varchar(50),
+    breed varchar(50),
+    color varchar(20),
+    owner_id integer REFERENCES owners
+);
+
+CREATE TABLE visits (
+    id SERIAL PRIMARY KEY,
+    checkin_date DATE,
+    checkout_date DATE,
+    pets_id integer REFERENCES pets
+);
