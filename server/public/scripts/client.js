f01feb49fb975500390cf164ec9cacfc97fc7795
Chris Stanton <cstanton0760@yahoo.com>
2017-03-02 16:03:42 -0600

    fixed appending method, started ajax post request

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,8 +1,6 @@
 console.log("this works");
 $(document).ready(function(){
 
-
-
 //registure button click listener
 $("#register").on("click", function(){
   event.preventDefault();
@@ -12,9 +10,14 @@ $("#register").on("click", function(){
 
   $("#ownerContainer").append('<p>' + fullName + '</p>');
 
+var ownerName = {};
     $.ajax({
       type: 'POST',
       url: '/ownername'
+      data: ownerName
+      success: function() {
+
+      }
     })
 });//end of registure listener
 
@@ -22,7 +25,7 @@ $("#register").on("click", function(){
 //addpet button click listener
 $("#addPetButton").on("click", function(){
   event.preventDefault();
-  var petName = $("#pName").val();
+  var petName = $("#petName").val();
   var petColor = $("#petColor").val();
   var petBreed = $("#breed").val();
 
@@ -40,7 +43,7 @@ $("#addPetButton").on("click", function(){
 $("table").on("click", ".goButton", function() {
   var thisPetId = $(this).parent().parent().children()
   $.ajax({
-    type: 'POST',
+    type: 'GET',
     url: '/save/' + thisPetId
   })
 }); //end on go button click
