f8b18d2da5bc22becc04c150b9077d1e8a2f8409
Edmund Wippler <ed.wippler@gmail.com>
2017-03-02 16:16:53 -0600

    updated get request on newpets.js

--- server/app.js
+++ server/app.js
@@ -2,11 +2,14 @@ var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
 var pg = require('pg');
+var newPet = require('./routes/newpet.js'); 
 var port = 5000;
 
 app.use(express.static('server/public'));
 
 app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/newpet', newPet);
 
 app.listen(port, function(){
   console.log('listening on port:', port);
